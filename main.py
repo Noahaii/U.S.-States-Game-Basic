f84b3209ea370c192c3a434db8294bd8091e229a
Noah Inocencio <ninocencio73@gmail.com>
2022-07-07 13:05:04 -0700

    used list comprehension

--- main.py
+++ main.py
@@ -11,15 +11,13 @@ t.shape(image)
 data = pd.read_csv("50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
-missing_states = []
 
 answer = screen.textinput("Guess a State", "What is a state").title()
 while len(guessed_states) < 50:
     if answer == "Exit":
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in guessed_states]
         missing_df = pd.DataFrame(missing_states)
+        missing_df.to_csv("states_to_learn.csv")
         break
     if answer in all_states:
         answer_data = data[data.state == answer]
@@ -33,7 +31,4 @@ while len(guessed_states) < 50:
         if answer not in guessed_states:
             guessed_states.append(answer)
     answer = screen.textinput(f"{len(guessed_states)}/50 States", "What is another state").title()
-print(f"your guessed list: {guessed_states}")
-missing_df.to_csv("states_to_learn.csv")
 t.mainloop()
-
